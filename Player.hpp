1c3d7db849e2cdc41bc9a7343b027262c6539903
AmitKarach <karach.amit30@gmail.com>
2021-05-03 22:06:30 +0300

    tests

--- Player.hpp
+++ Player.hpp
@@ -18,10 +18,10 @@ class Player{
             job =r;
         }
         Player& drive(City c);
-        virtual Player& fly_direct(City c);
-        Player& fly_charter(City c);
-        Player& fly_shuttle(City c);
-        virtual Player& build(); //building a reaserch lab in the city if there is a reaserch lab allready in this city we dont need to rais an error and the card stayies in the player hands
+        virtual Player& fly_direct(City c);// if u have the card of anther city u can go there by droping the city card
+        Player& fly_charter(City c);//if u have the card for the city u are in u can drop this card and go to tny city you want 
+        Player& fly_shuttle(City c);//if there is a research lab in this city u can move to any city with a research lab
+        virtual Player& build(); //building a research lab in the city, if there is a research lab allready in this city we dont need to rais an error and the card stayies in the player hands
         virtual Player& discover_cure(Color c);
         //lower one block of ilnace from the city
         virtual Player& treat(City c);
